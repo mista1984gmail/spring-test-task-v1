684b090093b0009a91a9c8ee7eb2da60ff961312
mista1984gmail <mista1984@gmail.com>
2024-02-01 13:54:53 +0300

    Initial commit

---
+++ src/main/java/com/example/springtesttaskv1/SpringTestTaskV1Application.java
@@ -0,0 +1,13 @@
+package com.example.springtesttaskv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestTaskV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestTaskV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtesttaskv1/SpringTestTaskV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtesttaskv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestTaskV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
